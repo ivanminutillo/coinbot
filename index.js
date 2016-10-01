e0851f6607cb9fdb3e41e2a350174dbc7076cec7
ivan minutillo <minutillo.ivan@gmail.com>
2016-10-01 15:08:33 +0200

    test

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const Telegram = require('telegram-node-bot')
+const TelegramBaseController = Telegram.TelegramBaseController
+const TextCommand = Telegram.TextCommand
+const tg = new Telegram.Telegram('279000152:AAFb5pq0V9e2eZM0TmWq1tcxjMtCL1iMCuQ')
+
+class PingController extends TelegramBaseController {
+  /**
+  * @param Scope $
+  */
+  pingHandler($) {
+    $.sendMessage('pong')
+  }
+
+  get routes() {
+    return {
+      'pingCommand' : 'pingHandler'
+    }
+  }
+}
+
+tg.router
+  .when(
+    new TextCommand('ping', 'pingCommand'),
+    new PingController()
+  )
